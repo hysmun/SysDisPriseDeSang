53aeae65ba7f4532f4f4e34f81af2bbb5f07a620
hysmun <throdlemassif@hotmail.com>
2017-12-27 17:35:25 +0100

    Merge origin/master
    Conflicts:
    	FilmGui/src/filmgui/FilmGui.java

---
+++ PriseDeSangLibrary/src/DataBaseLibrary/DBUtilities.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DataBaseLibrary;
+
+//import oracle.jdbc.driver.OracleDriver;
+import java.sql.*;
+import static java.sql.ResultSet.CONCUR_READ_ONLY;
+import static java.sql.ResultSet.TYPE_SCROLL_INSENSITIVE;
+
+/**
+ *
+ * @author 'Toine
+ */
+public class DBUtilities {
+    public static int SQL = 2;
+    
+    private int typeConnection;
+    private String nameConnection;
+    private String login;
+    private String motdepasse;
+    private Connection con;
+    private Statement instruc;
+
+    public DBUtilities(String plogin, String pmotdepasse, String ip, int port) throws ClassNotFoundException, Exception {
+        String tmpCon;
+        String nomDB = "bdprisedesang";
+        this.typeConnection = SQL;
+        
+        nameConnection = "org.gjt.mm.mysql.Driver";
+        
+        Class.forName(nameConnection);
+        login = plogin;
+        motdepasse = pmotdepasse;
+        
+        tmpCon = "jdbc:" + "mysql://"+ip+":"+port+"/"+nomDB;
+        con = DriverManager.getConnection(tmpCon, login, motdepasse);
+        instruc =  con.createStatement(TYPE_SCROLL_INSENSITIVE, CONCUR_READ_ONLY);
+    }
+    
+    public ResultSet query(String pquery) throws SQLException
+    {
+        return instruc.executeQuery(pquery);
+    }
+    
+    public int update(String pupdate) throws SQLException
+    {
+        return instruc.executeUpdate(pupdate);
+    }
+    
+    public boolean execute(String pupdate) throws SQLException
+    {
+        return instruc.execute(pupdate);
+    }
+    
+    //<editor-fold defaultstate="collapsed" desc="Getter">
+    public String getNameConnection() {
+        return nameConnection;
+    }
+    
+    public int getTypeConnection() {
+        return typeConnection;
+    }
+    
+    public String getLogin() {
+        return login;
+    }
+    
+    public String getMotdepasse() {
+        return motdepasse;
+    }
+    
+    public Connection getCon() {
+        return con;
+    }
+//</editor-fold>
+}
