ba2c62b6d3fcaaf371f563b923f141962d634527
Morghen <remy.mauhin@gmail.com>
2018-01-10 14:05:36 +0100

    Merge origin/master
    Conflicts:
    	properties/client.properties

---
+++ EjbPriseDeSang/src/java/EjbPriseDeSang/EjbLogin.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EjbPriseDeSang;
+
+import javax.annotation.security.DeclareRoles;
+import javax.annotation.security.RolesAllowed;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Morghen
+ */
+@Stateless
+@DeclareRoles("arole")
+public class EjbLogin implements EjbLoginRemote {
+
+    @RolesAllowed("arole")
+    @Override
+    public String doIt(String p) {
+        
+        return p;
+    }
+}
